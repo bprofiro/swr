dbf00fd9147f6308f168543b515d8d07660e9a54
Brenda <brendaprofiro@gmail.com>
2021-04-24 12:40:35 -0300

    chore: Create nagivation in app logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Image } from 'react-native';
 
+import { useNavigation } from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
 import logoImg from '../../assets/logo.png';
 
 import { Input } from '../Input';
 
 import { Container } from './styles';
 
-export const Header = () => (
-  <Container>
-    <Image source={logoImg} />
+export const Header = () => {
+  const navigation = useNavigation();
 
-    <Input />
-  </Container>
-);
+  return (
+    <Container>
+      <TouchableOpacity onPress={() => navigation.navigate('Users')}>
+        <Image source={logoImg} />
+      </TouchableOpacity>
+
+      <Input />
+    </Container>
+  );
+};
